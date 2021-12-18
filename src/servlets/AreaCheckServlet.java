5afe64aa57995ce0146f4e2d758bc3ec5b372464
neiton <neitonmatisso@gmail.com>
2021-12-18 13:59:08 +0300

    add validation on server side

--- src/servlets/AreaCheckServlet.java
+++ src/servlets/AreaCheckServlet.java
@@ -2,9 +2,10 @@ package servlets;
 
 import helpers.JsonParser;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.DoubleStream;
 import javax.servlet.ServletContext;
 import models.ResultData;
 import java.io.IOException;
@@ -17,41 +18,50 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AreaCheckServlet extends HttpServlet {
-  List resultsFromAllResponses = Collections.synchronizedList(new ArrayList<>());
+  List<ResultData> resultsFromAllResponses = Collections.synchronizedList(new ArrayList<>());
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    String[] xArrStr = (String[]) req.getAttribute("xArr");
-    double[] xArr = new double[xArrStr.length];
-    for (int i = 0; i < xArrStr.length; i++) {
-      xArr[i] = Double.parseDouble(xArrStr[i]);
-    }
-    double y = (double)req.getAttribute("y");
-    double r = (double)req.getAttribute("r");
+      PrintWriter output = resp.getWriter();
+      try {
+        String[] xArrStr = (String[]) req.getAttribute("xArr");
+        double[] xArr = new double[xArrStr.length];
+        for (int i = 0; i < xArrStr.length; i++) {
+          xArr[i] = Double.parseDouble(xArrStr[i]);
+        }
+        double y = (double) req.getAttribute("y");
+        double r = (double) req.getAttribute("r");
+        if (!validateR(r)) {
+          throw new NumberFormatException();
+        }
+        Date currentDate = new Date();
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("HH:mm:ss");
+        String currentTime = dateFormatter.format(currentDate);
+        List<ResultData> listOfResult = new ArrayList<>();
 
-    Date currentDate = new Date();
-    SimpleDateFormat dateFormatter = new SimpleDateFormat("HH:mm:ss");
-    String currentTime = dateFormatter.format(currentDate);
-    List<ResultData> listOfResult = new LinkedList<>();
+        for (double x : xArr) {
+          long startTime = System.nanoTime();
+          String answer = checkHit(x, y, r) ? "YES" : "NO";
+          long processingTime = System.nanoTime() - startTime;
+          ResultData result = new ResultData(x, y, r, answer, currentTime, processingTime);
+          listOfResult.add(result);
+        }
 
-    for (double x : xArr){
-      long startTime = System.nanoTime();
-      String answer = checkHit(x, y, r) ? "YES" : "NO";
-      long processingTime = System.nanoTime() - startTime;
-      ResultData result = new ResultData(x, y, r, answer, currentTime, processingTime);
-      listOfResult.add(result);
-    }
+        ServletContext context = this.getServletContext();
+        if (context.getAttribute("results") == null) {
+          context.setAttribute("results", resultsFromAllResponses);
+        }
+        resultsFromAllResponses.addAll(listOfResult);
+        context.setAttribute("results", resultsFromAllResponses);
 
-    ServletContext context = this.getServletContext();
-    if (context.getAttribute("results") == null) {
-      context.setAttribute("results", resultsFromAllResponses);
-    }
-    resultsFromAllResponses.addAll(listOfResult);
-    context.setAttribute("results", resultsFromAllResponses);
-
-    JsonParser jsonParser = new JsonParser();
-    PrintWriter output = resp.getWriter();
-    output.println(jsonParser.toJson(listOfResult));
+        JsonParser jsonParser = new JsonParser();
+        output.println(jsonParser.toJson(listOfResult));
+      } catch (NumberFormatException e) {
+        output.print("Invalid-data");
+        e.printStackTrace();
+      } finally {
+        output.close();
+      }
   }
 
   private boolean checkHit(double x, double y, double r) {
@@ -66,23 +76,21 @@ public class AreaCheckServlet extends HttpServlet {
   }
 
   private boolean checkRectangleHit(double x, double y, double r) {
-    boolean result = false;
-    if (x <= r && Math.abs(y) <= r/2)
-      result = true;
-    return result;
+    return x <= r && Math.abs(y) <= r / 2;
   }
 
   private boolean checkTriangleHit(double x, double y, double r) {
-    boolean result = false;
-    if (r + x >= 2 * Math.abs(y))
-      result = true;
-    return result;
+    return r + x >= 2 * Math.abs(y);
   }
 
   private boolean checkCircleHit(double x, double y, double r) {
-    boolean result = false;
-    if (Math.sqrt(x*x + y*y) <= r/2)
-      result = true;
-    return result;
+    return Math.sqrt(x * x + y * y) <= r / 2;
+  }
+
+  private boolean validateR(double r) {
+    List<Double> correctRValues = Arrays.asList(1d, 1.5d, 2d, 2.5d, 3d);
+    if (!correctRValues.contains(r))
+      return false;
+    return true;
   }
 }
--- src/servlets/ControllerServlet.java
+++ src/servlets/ControllerServlet.java
@@ -11,18 +11,24 @@ public class ControllerServlet extends HttpServlet {
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    if (req.getParameterValues("xArr[]")!= null && req.getParameter("y")!= null
-        && req.getParameter("r")!= null) {
-      String[] xArrStr = req.getParameterValues("xArr[]");
-      double y = Double.parseDouble(req.getParameter("y"));
-      double r = Double.parseDouble(req.getParameter("r"));
+    try {
+      resp.setCharacterEncoding("UTF-8");
+      if (req.getParameterValues("xArr[]") != null && req.getParameter("y") != null
+          && req.getParameter("r") != null) {
+        String[] xArrStr = req.getParameterValues("xArr[]");
+        double y = Double.parseDouble(req.getParameter("y"));
+        double r = Double.parseDouble(req.getParameter("r"));
 
-      req.setAttribute("xArr", xArrStr);
-      req.setAttribute("y", y);
-      req.setAttribute("r", r);
-      getServletContext().getRequestDispatcher("/check").forward(req, resp);
-    } else {
-      getServletContext().getRequestDispatcher("/reload").forward(req, resp);
+        req.setAttribute("xArr", xArrStr);
+        req.setAttribute("y", y);
+        req.setAttribute("r", r);
+        getServletContext().getRequestDispatcher("/check").forward(req, resp);
+      } else {
+        getServletContext().getRequestDispatcher("/reload").forward(req, resp);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      getServletContext().getRequestDispatcher("/index.jsp").forward(req,resp);
     }
   }
 }
